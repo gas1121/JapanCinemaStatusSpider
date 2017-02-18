3fa2a8403a5966070a2092728f21489dc8a77002
gas1121 <jtdzhx@gmail.com>
2017-02-18 21:38:21 +0800

    cinema spider support cinemas that have sub page.

--- scrapyproject/scrapyproject/spiders/toho_cinema.py
+++ scrapyproject/scrapyproject/spiders/toho_cinema.py
@@ -30,24 +30,47 @@ class TohoSpider(scrapy.Spider):
         cinema_name = response.xpath(
             '//h1[@class="c-page_heading is-lv-01"]'
             '/span/text()').extract_first()
-        all_screen_list = response.xpath(
-            '//table[@class="c-table01 __table"]/tbody/tr')
-        # except total seats line
-        all_screen_list = all_screen_list[:-1]
         cinema = Cinema()
         cinema['name'] = cinema_name
         cinema['screens'] = {}
+        # some cinemas have detail page and need to forward
+        sub_page_list = response.xpath(
+            '//section[@class="about"]//a[@class="link bold"]/@href').extract()
+        if sub_page_list:
+            curr_page_url = response.urljoin(sub_page_list[0])
+            request = scrapy.Request(curr_page_url,
+                                     callback=self.parse_sub_cinema)
+            request.meta['cinema'] = cinema
+            request.meta['remain_page_list'] = sub_page_list[1:]
+            yield request
+        else:
+            self.parse_seat_number_list(response, cinema)
+            yield cinema
+
+    def parse_sub_cinema(self, response):
+        self.parse_seat_number_list(response, response.meta['cinema'])
+        remain_page_list = response.meta['remain_page_list']
+        if remain_page_list:
+            curr_page_url = remain_page_list[0]
+            request = scrapy.Request(curr_page_url,
+                                     callback=self.parse_sub_cinema)
+            request.meta['remain_page_list'] = remain_page_list[1:]
+            request.meta['cinema'] = response.meta['cinema']
+            yield request
+        else:
+            yield response.meta['cinema']
+
+    def parse_seat_number_list(self, response, cinema):
+        all_screen_list = response.xpath(
+            '//table[contains(@class,"c-table01")]/tbody/tr')
+        # except total seats line
+        all_screen_list = all_screen_list[:-1]
         for curr_screen in all_screen_list:
             screen_name = curr_screen.xpath(
                 './td[1]/text()').extract_first()
-            print(screen_name)
             if screen_name is not None:
                 screen_seat_number_list = curr_screen.xpath(
                     './td[2]/text()').re(r'([0-9]+)[\+\＋]\(([0-9]+)\)')
-                print(curr_screen.xpath(
-                    './td[2]/text()').extract_first())
-                print(screen_seat_number_list)
                 screen_seat_number = (int(screen_seat_number_list[0])
                                       + int(screen_seat_number_list[1]))
                 cinema['screens'][screen_name] = screen_seat_number
-        yield cinema
