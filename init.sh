e96bcffa820955444774d46bb461a0574949319b
gas1121 <jtdzhx@gmail.com>
2017-03-02 09:48:35 +0800

    Use parser to read config in bash.

--- init.sh
+++ init.sh
@@ -1,4 +1,10 @@
-#! /bin/sh
+#! /bin/bash
 #
 # project init script
+# ini parser from https://github.com/albfan/bash-ini-parser
 #
+
+. script/bash-ini-parser
+cfg_parser init.cfg
+#cfg_section_postgres
+#$POSTGRES_DB
