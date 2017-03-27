475597e57cb8cfe3a82aaa4c90add336ca12118d
gas1121 <jtdzhx@gmail.com>
2017-03-27 20:52:17 +0800

    movix spider develop

--- scrapyproject/spiders/movix.py
+++ scrapyproject/spiders/movix.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import re
 import copy
 import arrow
 import scrapy
@@ -40,8 +41,6 @@ class MovieSpider(ShowingSpider):
             else:
                 curr_cinema_url = response.urljoin(curr_cinema_url)
             cinema_name = standardize_cinema_name(cinema_name)
-            # TODO
-            print(cinema_name)
             if not self.is_cinema_crawl([cinema_name]):
                 continue
             request = scrapy.Request(
@@ -51,80 +50,83 @@ class MovieSpider(ShowingSpider):
             yield request
 
     def parse_cinema(self, response):
-        # TODO
-        print('parse_cinema')
-        return
-        schedule_url = response.xpath(
-            '//div[@class="viewport"]//a'
-            '[contains(@href,"' + self.date + '")]').extract_first()
-        schedule_url = response.urljoin(schedule_url)
-        cinema_name = response.xpath('//header/h1/a/img/@alt').extract_first()
-        if not self.is_cinema_crawl([cinema_name]):
-            return
+        """
+        get cinema code from homepage's javascript
+        """
+        script_text = response.xpath(
+            '//script[contains(.,"thnumber")]/text()').extract_first()
+        thnumber = re.findall(r'\d+', script_text)[0]
+        schedule_url = self.generate_cinema_schedule_url(
+            thnumber, self.date)
+        request = scrapy.Request(
+            schedule_url, encoding='utf-8', callback=self.parse_shechedule)
+        request.meta["cinema_name"] = response.meta["cinema_name"]
+        request.meta["cinema_site"] = response.meta["cinema_site"]
+        yield request
+
+    def generate_cinema_schedule_url(self, thnumber, show_day):
+        """
+        schedule url for single cinema, all movies of curr cinema
+        """
+        url = 'http://www.smt-cinema.com/schedule/pc/s0100_'\
+              '{thnumber}_{show_day}.html'.format(
+                  thnumber=thnumber, show_day=show_day)
+        return url
+
+    def parse_shechedule(self, response):
         data_proto = Showing()
-        data_proto['cinema_name'] = cinema_name
+        data_proto['cinema_name'] = response.meta['cinema_name']
         data_proto["cinema_site"] = response.meta['cinema_site']
         result_list = []
-        movie_section_list = response.xpath('//ul[@id="dailyList"]/li')
+        movie_section_list = response.xpath('//div[@class="scheduleBox"]')
         for curr_movie in movie_section_list:
             self.parse_movie(response, curr_movie, data_proto, result_list)
         for result in result_list:
             if result:
                 yield result
 
-    def generate_cinema_schedule_url(self, cinema_name_en, show_day):
-        """
-        schedule url for single cinema, all movies of curr cinema
-        """
-        url = 'http://109cinemas.net/{cinema_name_en}/schedules/{date}.html'\
-              '/daily.php?date={date}'.format(
-                  cinema_name_en=cinema_name_en, date=show_day)
-        return url
-
     def parse_movie(self, response, curr_movie, data_proto, result_list):
         """
         parse movie showing data
         """
-        print('parse_movie')
-        title = curr_movie.xpath('./h3/span/a[1]/text()').extract_first()
+        title = curr_movie.xpath(
+            './/div[@class="MovieTitle1"]//a/text()').extract_first()
         title_list = [title]
         if not self.is_movie_crawl(title_list):
             return
         movie_data_proto = copy.deepcopy(data_proto)
         movie_data_proto['title'] = title
-        screen_section_list = curr_movie.xpath('./ul/li')
-        for curr_screen in screen_section_list:
-            self.parse_screen(response, curr_screen,
-                              movie_data_proto, result_list)
-
-    def parse_screen(self, response, curr_screen, data_proto, result_list):
-        print('parse_screen')        
-        screen_data_proto = copy.deepcopy(data_proto)
-        screen_name = curr_screen.xpath('./p/a/img/@alt').extract_first()
-        screen_data_proto['screen'] = standardize_screen_name(
-            screen_name, screen_data_proto['cinema_name'])
-        show_section_list = curr_screen.xpath('./ol/li')
-        for curr_showing in show_section_list:
+        showing_section_list = curr_movie.xpath(
+            './/td[contains(@onmouseover,"eventover")]')
+        for curr_showing in showing_section_list:
             self.parse_showing(response, curr_showing,
-                               screen_data_proto, result_list)
+                               movie_data_proto, result_list)
 
     def parse_showing(self, response, curr_showing, data_proto, result_list):
         def parse_time(time_str):
             time = time_str.split(":")
             return (int(time[0]), int(time[1]))
-        print('parse_showing')
+        print("parse_showing")
 
         showing_data_proto = copy.deepcopy(data_proto)
+        screen_name = curr_showing.xpath('./p/text()').extract_first()
+        showing_data_proto['screen'] = standardize_screen_name(
+            screen_name, showing_data_proto['cinema_name'])
         start_time = curr_showing.xpath(
-            './div/ol/li[@class="startTime"]/text()').extract_first()
+            './/span[@class="strong fontXL"]/text()').extract_first()
         start_hour, start_minute = parse_time(start_time)
         showing_data_proto['start_time'] = self.get_time_from_text(
             start_hour, start_minute)
         end_time = curr_showing.xpath(
-            './div/ol/li[@class="endTime"]/text()').extract_first()[1:]
+            './/span[@class="strong fontXL"]/../text()').extract_first()[1:]
         end_hour, end_minute = parse_time(end_time)
         showing_data_proto['end_time'] = self.get_time_from_text(
             end_hour, end_minute)
+        # TEST
+        print(showing_data_proto['screen'])
+        print(showing_data_proto['start_time'])
+        print(showing_data_proto['end_time'])
+        return
         book_status = curr_showing.xpath(
             './div/ul/li[@class="uolIcon"]//img[1]/@src').extract_first()
         showing_data_proto['book_status'] = \
