4edd65b6d822eadae733ba8f4dd6c5d48f184e2e
gas1121 <jtdzhx@gmail.com>
2017-04-04 11:43:47 +0800

    schedule script developing.

--- run.py
+++ run.py
@@ -2,34 +2,36 @@
 
 import time
 import schedule
-from twisted.internet import reactor, defer
+from twisted.internet import reactor
 from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
 from scrapy.utils.project import get_project_settings
 
 
 def crawl_job():
-    @defer.inlineCallbacks
-    def crawl(runner):
-        yield runner.crawl('toho_v2')
-        yield runner.crawl('movix', keep_old_data=True)
-        yield runner.crawl('aeon', keep_old_data=True)
-        yield runner.crawl('united', keep_old_data=True)
-        yield runner.crawl('kinezo', keep_old_data=True)
-        yield runner.crawl('site109', keep_old_data=True)
-        yield runner.crawl('cinemasunshine', keep_old_data=True)
-        yield runner.crawl('forum', keep_old_data=True)
-        yield runner.crawl('korona', keep_old_data=True)
-        reactor.stop()
-    configure_logging()
-    runner = CrawlerRunner(get_project_settings())
-    crawl(runner)
+    print("crawl_job")
+    settings = get_project_settings()
+    configure_logging(settings=settings)
+    runner = CrawlerRunner(settings)
+
+    runner.crawl('toho_v2', keep_old_data=True)
+    runner.crawl('movix', keep_old_data=True)
+    runner.crawl('aeon', keep_old_data=True)
+    runner.crawl('united', keep_old_data=True)
+    runner.crawl('kinezo', keep_old_data=True)
+    runner.crawl('site109', keep_old_data=True)
+    runner.crawl('cinemasunshine', keep_old_data=True)
+    runner.crawl('forum', keep_old_data=True)
+    runner.crawl('korona', keep_old_data=True)
+    d = runner.join()
+    d.addBoth(lambda _: reactor.stop())
+
     reactor.run()
-    return schedule.CancelJob
 
 
 if __name__ == '__main__':
-    schedule.every().day.at('13:00').do(crawl_job)
+    print("__main__")
+    schedule.every().day.at('02:09').do(crawl_job)
     while True:
         schedule.run_pending()
         time.sleep(1)
