33a6fa7e9c4347a85af42f127b15fce2b321ac40
gas1121 <jtdzhx@gmail.com>
2017-07-27 20:59:42 +0800

    update travis script

--- travis/docker.sh
+++ travis/docker.sh
@@ -3,8 +3,8 @@
 set -e
 
 # Build docker image
-sudo docker build --rm=true --file docker/crawler/Dockerfile --tag=gas1121/JapanCinemaStatusSpider:crawler-test .
-sudo docker build --rm=true --file docker/scheduler/Dockerfile --tag=gas1121/JapanCinemaStatusSpider:scheduler-test .
+sudo docker build --rm=true --file docker/crawler/Dockerfile --tag=gas1121/japancinemastatusspider:crawler-test .
+sudo docker build --rm=true --file docker/scheduler/Dockerfile --tag=gas1121/japancinemastatusspider:scheduler-test .
 
 # start target service for testing
 sudo docker-compose -f travis/docker-compose.test.yml up -d
@@ -12,13 +12,20 @@ sudo docker-compose -f travis/docker-compose.test.yml up -d
 # waiting 10 secs
 sleep 10
 
+# install package for test
+sudo docker-compose -f travis/docker-compose.test.yml exec pip install coverage coveralls
+
+
 # run tests
-sudo docker-compose -f travis/docker-compose.test.yml exec scheduler nosetests -v
+sudo docker-compose -f travis/docker-compose.test.yml exec scheduler nosetests -v --with-coverage --cover-erase
 sudo docker-compose -f travis/docker-compose.test.yml exec scheduler python tests/online.py
+# send coverage report
+sudo docker-compose -f travis/docker-compose.test.yml exec coveralls
+# TODO combine different module coverage reports
 
 # spin down compose
 sudo docker-compose -f travis/docker-compose.test.yml down
 
 # remove 'test' images
-sudo docker rmi gas1121/JapanCinemaStatusSpider:crawler-test
-sudo docker rmi gas1121/JapanCinemaStatusSpider:scheduler-test
+sudo docker rmi gas1121/japancinemastatusspider:crawler-test
+sudo docker rmi gas1121/japancinemastatusspider:scheduler-test
