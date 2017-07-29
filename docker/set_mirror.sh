3434f7f00e87d85f87dd8c28aad376e2d0f00e40
gas1121 <jtdzhx@gmail.com>
2017-07-29 14:39:35 +0800

    update mirror script

--- docker/set_mirror.sh
+++ docker/set_mirror.sh
@@ -1,6 +1,7 @@
 if [ $USE_MIRROR -eq 1 ]; then
     sed -i 's/httpredir.debian.org/mirrors.ustc.edu.cn/g' /etc/apt/sources.list;
     sed -i 's/deb.debian.org/mirrors.ustc.edu.cn/g' /etc/apt/sources.list;
+    sed -i 's|security.debian.org|mirrors.ustc.edu.cn/debian-security|g' /etc/apt/sources.list;
     mkdir -p ~/.pip;
     echo "[global]" > ~/.pip/pip.conf;
     echo "format = columns" >> ~/.pip/pip.conf;
