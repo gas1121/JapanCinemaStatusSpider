0c029380d809418fa4e05899136ca90d2ede7890
gas1121 <jtdzhx@gmail.com>
2017-08-03 10:05:08 +0800

    update online tests for data_processor

--- data_processor/tests/online.py
+++ data_processor/tests/online.py
@@ -1,10 +1,14 @@
 import unittest
 from mock import MagicMock, patch
 import os
+import json
 
 from sqlalchemy import Column, Integer
 from sqlalchemy.engine.url import URL
 from sqlalchemy_utils import drop_database
+from kafka_monitor import KafkaMonitor
+from scutils.method_timer import MethodTimer
+
 from models import (DeclarativeBase, create_table,
                     drop_table_if_exist, db_connect)
 from models.cinema import Cinema
@@ -104,6 +108,62 @@ class TestDbManageHandler(unittest.TestCase):
 
     def tearDown(self):
         drop_database(self.url)
+
+
+# setup custom class to handle our requests
+class CustomHandler(MagicMock):
+    # use dbmanage handler's schema just for test
+    schema = "dbmanage_schema.json"
+
+    def handle(self, dict):
+        pass
+
+
+class TestKafkaMonitor(unittest.TestCase):
+    def setUp(self):
+        self.kafka_monitor = KafkaMonitor("localsettings.py")
+        new_settings = self.kafka_monitor.wrapper.load("localsettings.py")
+        new_settings['KAFKA_INCOMING_TOPIC'] = "jcss.incoming_test"
+        new_settings['KAFKA_CONSUMER_TIMEOUT'] = 5000
+        new_settings['STATS_TOTAL'] = False
+        new_settings['STATS_PLUGINS'] = False
+        new_settings['PLUGINS'] = {
+            'plugins.dbmanage_handler.DbManageHandler': None,
+            'tests.online.CustomHandler': 100,
+        }
+
+        self.kafka_monitor.wrapper.load = MagicMock(return_value=new_settings)
+        self.kafka_monitor.setup()
+
+        @MethodTimer.timeout(10, False)
+        def timer():
+            self.kafka_monitor._setup_kafka()
+            return True
+
+        retval = timer()
+        if not retval:
+            self.fail("Unable to connect to Kafka")
+        self.kafka_monitor._load_plugins()
+        self.kafka_monitor._setup_stats()
+        self.assertTrue(100 in self.kafka_monitor.plugins_dict)
+        self.assertTrue(isinstance(
+            self.kafka_monitor.plugins_dict[100]['instance'], CustomHandler))
+
+    def test_feed(self):
+        json_req = "{\"action\":\"init\",\"target\":\"all\"}"
+        parsed = json.loads(json_req)
+        # ensure the group id is present so we pick up the 1st message
+        self.kafka_monitor._process_messages()
+        self.kafka_monitor.feed(parsed)
+
+    def test_run(self):
+        handler = self.kafka_monitor.plugins_dict[100]['instance']
+        handler.handle = MagicMock()
+        self.kafka_monitor._process_messages()
+        handler.handle.assert_called_once()
+
+    def tearDown(self):
+        self.kafka_monitor.close()
 
 
 if __name__ == '__main__':
