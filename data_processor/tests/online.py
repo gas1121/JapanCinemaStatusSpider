2edbf3904728a1f0ba5cf2218b53f820b865eae9
gas1121 <jtdzhx@gmail.com>
2017-08-05 17:42:20 +0800

    change default db in travis test

--- data_processor/tests/online.py
+++ data_processor/tests/online.py
@@ -10,8 +10,6 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import drop_database
 from kafka_monitor import KafkaMonitor
 from scutils.method_timer import MethodTimer
-from models.movie import Movie
-"""
 from models import (DeclarativeBase, create_table,
                     drop_table_if_exist, db_connect, add_item_to_database)
 from models.cinema import Cinema
@@ -21,7 +19,7 @@ from models.showing_booking import ShowingBooking
 from plugins.dbmanage_handler import DbManageHandler
 from plugins.crawled_movie_handler import CrawledMovieHandler
 from plugins.crawled_cinema_handler import CrawledCinemaHandler
-"""
+
 
 class DatabaseMixin(object):
     def setUp(self):
@@ -38,18 +36,17 @@ class DatabaseMixin(object):
     def tearDown(self):
         drop_database(self.url)
 
-"""
+
 class TestTable(DeclarativeBase):
     __tablename__ = "test_table"
 
     id = Column(Integer, primary_key=True)
     data = Column('data', String)
-"""
+
 
 class TestModels(DatabaseMixin, unittest.TestCase):
     def test_db_connect(self):
         self.assertEqual(1, 1)
-"""
         engine = db_connect(self.database)
         self.assertEqual(engine.name, 'postgresql')
 
@@ -207,7 +204,7 @@ class TestKafkaMonitor(unittest.TestCase):
 
     def tearDown(self):
         self.kafka_monitor.close()
-"""
+
 
 if __name__ == '__main__':
     unittest.main()
